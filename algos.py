fc8c38e9c311d41a61b478835f91e1903ab20414
NeilSCGH <neil.segard@gmail.com>
2020-03-07 13:22:17 +0100

    maj

--- algos.py
+++ algos.py
@@ -1,24 +1,40 @@
-def f(x):
-    return 2*x*x +x
+import numpy as np
 
-def fp(x):
-    return 4*x +1
+def g(x):
+    return np.square(x)
+
+def gp(x):
+    return np.multiply(2,x)
 
 
-def Newton(x, e):
+def Newton(f, fp, x, e):
     xold=x
     x=xold-f(xold)/fp(xold)
 
     while abs(xold-x)>e:
-        print(x," ",xold)
+        #print(x," ",xold)
         xold=x
         x=xold-f(xold)/fp(xold)
 
     return x
 
+def descente(x):
+    xold=x
+    x=xold-f(xold)/fp(xold)
+
+    while abs(xold-x)>e:
+        print(x," ",xold)
+
+        d=1
+        a=1
+
+        xold=x
+        x=xold + a*d
+
+    return x
 
 
-print(Newton(1, 0.001))
 
 
 
+print(Newton(g, gp, 1, 0.001))
