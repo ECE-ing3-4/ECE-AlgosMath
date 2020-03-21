54f9d163fe01e00f49518ee3820b4147984a2a8b
NeilSCGH <neil.segard@gmail.com>
2020-03-22 00:38:58 +0100

    j'ai rendu le code un peu plus joli

--- DM2.py
+++ DM2.py
@@ -12,33 +12,32 @@ N = 8
 h=1/(N+1)
 
 X_first = np.random.randint(100,size=(N,1))
-A = 2 * np.eye(N).reshape(N,N)
+A = 2 * np.eye(N)
+
 for i in range(N):
     for j in range(N):
         if (j==i+1 or j==i-1):
             A[i,j]=-1
-A= A / (h)**2
-b = -8*np.ones(N).reshape(N,1)
 
+A= A / (h)**2
+B = -8*np.ones((N,1))
 
 def J(X):
-    return 0.5 * np.vdot(np.dot(A,X),X) + np.vdot(b,X)
+    return 0.5 * np.vdot(np.dot(A,X),X) + np.vdot(B,X)
+
 def grad_J(X):
-    return (np.vdot(b , np.ones(np.shape(A)[0])) + (np.vdot(np.dot(A,X),X) / norm_eucl(X, np.zeros(np.shape(X)[0]))))  # derivation de matrice
+    return np.vdot(B , np.ones(len(A))) + (np.vdot(np.dot(A,X),X) / norm_eucl(X, np.zeros(len(X))))  # derivation de matrice
 
 def prod_scal(X,Y):
     return np.vdot(X,Y)
 
 def norme_deux(X,Y):
-    n1= np.shape(X)
-    n2= np.shape(Y)
+    assert np.shape(X) == np.shape(Y)
     sum=0
-    if (n1 !=n2):
-        print("erreur dim")
-        return 0
-    else:
-        for i in range(n1):
-            sum+= (X[i]-Y[i])**2
+
+    for i in range(n1):
+        sum+= (X[i]-Y[i])**2
+
     return np.sqrt(sum)
 
 def norm_eucl(X,Y):
@@ -51,17 +50,8 @@ def projection(X,plan):
 
     return (prod_scal(X,plan)/norm)*plan
 
-G = np.zeros(N) #creation des gi dans un vecteur
-for i in range(N):
-    t = (i+1)*h
-    G[i] = -1 + max(0, -10 * (t-0.4)**(2) + 0.625)
-
-
-
 ## Algo du gradient descente projeté
-
 def gradientDescenteProjete(contraintes_g, precision, X0):
-
     X=deepcopy(X0)
     X1 = deepcopy(X)
     pas = 0.05 # arbitraire
@@ -87,25 +77,16 @@ def gradientDescenteProjete(contraintes_g, precision, X0):
         residu = norm_eucl(X1,Proj)
         #contraintes_g = G(X)
         compteur+=1
-    print(residu)
-    print(precision)
-    print(compteur)
+    # print(residu)
+    # print(precision)
+    # print(compteur)
     return X
 
 ## Algo d'Uzawa
-
 #1 mettre sous la forme CX <= g les contraintes C1
-
 C1 = -1 * np.eye(N)
 
-def g(X):
-    var = np.dot(C1,X)
-    for i in range(N):
-        var-=G[i]
-    return var
-
 #2 mettre sous la forme CX = 0 les contraintes C2
-
 C2 = np.zeros((N,N))
 for i in range(N):
     for j in range(N):
@@ -114,6 +95,17 @@ for i in range(N):
                 C2[i,(j+i)]=1
                 C2[i,(j+i+1)]=-1
 
+G = np.zeros(N) #creation des gi dans un vecteur
+for i in range(N):
+    t = (i+1)*h
+    G[i] = -1 + max(0, -10 * (t-0.4)**(2) + 0.625)
+
+def g(X):
+    var = np.dot(C1,X)
+    for i in range(N):
+        var-=G[i]
+    return var
+
 def h(X):
     return np.dot(C2,X)
 
@@ -121,13 +113,13 @@ def Lagrangien(X,L1,Mu):
     return J(X) + prod_scal(L1,h(X))+prod_scal(Mu,g(X))
 
 def Grad_x_Lagrangien(X,L1,Mu):
-    return np.dot(A,X)-b + np.dot(np.transpose(C2),L1) + np.dot(np.transpose(C1),Mu)
+    return np.dot(A,X)-B + np.dot(C2.T,L1) + np.dot(C1.T,Mu)
 
 def Grad_L1_Lagrangien(X,L1,Mu):
-    return np.dot(np.transpose(X),np.transpose(C2))
+    return np.dot(X.T,C2.T)
 
 def Grad_Mu_Lagrangien(X,L1,Mu):
-    return np.transpose(np.dot(C1,X) - g(X))
+    return (np.dot(C1,X) - g(X)).T
 
 #3 Programmer uzawa pour les contraintes d'inégalité
 def Uzawa_contrainte_ineg(X0, tolerance):
@@ -152,12 +144,13 @@ def Uzawa_contrainte_ineg(X0, tolerance):
         for i in range(N): #cherche le max entre 0 et les mu_i
             Mu1[i]=max(0,Mu1[i])
 
-        X1 = np.dot(np.linalg.inv(A) ,b - np.dot(C1.T,Mu))
+        X1 = np.dot(np.linalg.inv(A) ,B - np.dot(C1.T,Mu))
 
         test_x = norm_eucl(X1,X)
         test_mu = norm_eucl(Mu,Mu1)
         compteur+=1
-    print(compteur)
+
+    #print(compteur)
     return X1,Mu1
 
 #4 Programmer uzawa pour les contraintes d'égalité
@@ -176,17 +169,16 @@ def Uzawa_contrainte_eg(X0, tolerance):
         tmp=(pas * h(X)).T
         L1 = L1 + tmp
 
-        X1 = np.dot(np.linalg.inv(A) ,b - np.dot(C2.T,L.T))
+        X1 = np.dot(np.linalg.inv(A) ,B - np.dot(C2.T,L.T))
 
         test_x = norm_eucl(X1,X)
         test_l = norm_eucl(L,L1)
 
         compteur+=1
-    print(compteur)
+    #print(compteur)
     return X1,L1
 
-    #5 Programmer uzawa pour les contraintes mixtes
-
+#5 Programmer uzawa pour les contraintes mixtes
 def Uzawa_contrainte_mixtes(X0, tolerance):
     X1=deepcopy(X0)
 
@@ -217,21 +209,25 @@ def Uzawa_contrainte_mixtes(X0, tolerance):
         for i in range(N): #cherche le max entre 0 et les mu_i
             Mu1[i]=max(0,Mu1[i])
 
-        X1 = np.dot(np.linalg.inv(A) ,b - np.dot(C2.T,L.T) - np.dot(C1.T,Mu))
+        X1 = np.dot(np.linalg.inv(A) ,B - np.dot(C2.T,L.T) - np.dot(C1.T,Mu))
 
         test_x = norm_eucl(X1,X)
         test_l = norm_eucl(L,L1)
         test_mu = norm_eucl(Mu,Mu1)
 
         compteur+=1
-    print(compteur)
+
+    #print(compteur)
     return X1,L1
 
-
-
 ## Tests
-#gradientDescenteProjete(g(X_first),0.01,X_first)
+print(gradientDescenteProjete(g(X_first),0.01,X_first))
+print()
 
-Uzawa_contrainte_ineg(X_first,0.01)
-Uzawa_contrainte_eg(X_first,0.01)
-Uzawa_contrainte_mixtes(X_first,0.01)
+print(Uzawa_contrainte_ineg(X_first,0.01))
+print()
+
+print(Uzawa_contrainte_eg(X_first,0.01))
+print()
+
+print(Uzawa_contrainte_mixtes(X_first,0.01))
