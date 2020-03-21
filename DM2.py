aa40e483f9860c96166226c3d53b452350f24051
Alexis Direz <alex.direz@gmail.com>
2020-03-21 18:27:00 +0100

    search sol

--- DM2.py
+++ DM2.py
@@ -6,12 +6,17 @@
 ## Imports et fonctions de base:
 
 import numpy as np
+from copy import deepcopy
 
-X = np.array([1,7,3,4], float)
-A = np.arange(16).reshape(4,4)
-b = np.array(np.random.random_sample(4)).reshape(4,1)
-J = 0.5 * np.vdot(np.dot(A,X),X) + np.vdot(b,X)
-grad_J = (np.vdot(b , np.ones(np.shape(A)[0]) ) + (np.vdot(np.dot(A,X),X) / norm_eucl(X, np.zeros(np.shape(X)[0]))) )  # derivation de matrice
+N = 4
+
+X_first = np.array([1,7,3,4], float)
+A = np.arange(N**2).reshape(N,N)
+b = -8*np.ones(N).reshape(N,1)
+def J(X):
+    return 0.5 * np.vdot(np.dot(A,X),X) + np.vdot(b,X)
+def grad_J(X):
+    return (np.vdot(b , np.ones(np.shape(A)[0]) ) + (np.vdot(np.dot(A,X),X) / norm_eucl(X, np.zeros(np.shape(X)[0]))) )  # derivation de matrice
 
 def prod_scal(X,Y):
     return np.vdot(X,Y)
@@ -38,19 +43,46 @@ def projection(X,plan):
 
     return (prod_scal(X,plan)/norm)*plan
 
+def G(X):
+    n = len(X)
+    # print(n)
+    tab = np.zeros(n)
+    for i in range(n):
+        tab[i] = X[i] + X[i-1]*(-1)**i  #je chois les contraintes valant xi plus ou moins la valeur du xi precedent
+    return tab
+
 
 ## Algo du gradient descente projeté
-def gradientDescenteProjete(J, contraintes_g, precision, X0):
-    X=X0
-    Y=X0 + 7 #arbitraire de prendre 7
+def gradientDescenteProjete(contraintes_g, precision, X0):
+    X=deepcopy(X0)
+    Y=deepcopy(X0+7)  #arbitraire de prendre 7
     pas = 0.05 #aussi arbitraire
+    n1=np.shape(X)[0]
+    n2 = np.shape(contraintes_g)[0]
     residu = norm_eucl(X,Y)
+    Proj = np.zeros(np.shape(X0))
+    compteur = 0
     while(residu > precision):
-        X0-= pas * 1
-    return 1
+        X1 = deepcopy(X)
+        X-= pas * grad_J(X)
+        for i in range(n2):
+            if (X[i] < contraintes_g[i]):
+                Proj[i]=contraintes_g[i]
+            else:
+                Proj[i]=X[i]
+        if(n2<n1):
+            for i in range(n2,n1,1):
+                Proj[i]=X[i]
+        residu = norm_eucl(X,Proj)
+        #contraintes_g = G(X)
+        compteur+=1
+    print(residu)
+    print(precision)
+    print(compteur)
+    return X
 
 ## Algo d'Uzawa
-    #1 mettre sous la forme CX<= g les contraintes c1
+    #1 mettre sous la forme CX <= g les contraintes C1
     #2 mettre sous la forme CX = 0 les contraintes C2
     #3 Programmer uzawa pour les contraintes d'inégalité
     #4 Programmer uzawa pour les contraintes d'égalité
